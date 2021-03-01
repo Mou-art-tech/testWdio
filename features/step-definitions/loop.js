55c616fb29a53d0012ea70591c2d4fcb2973bc42
Mou-art-tech <moumitabasak1990@gmail.com>
2021-03-01 11:57:22 +0530

    initial commit

---
+++ features/step-definitions/loop.js
@@ -0,0 +1,60 @@
+// const a = [
+//     { prop1: 100, prop2: false, prop3: 'hello'},
+//     { prop1: 101, prop2: true, prop3: 'hello 1'},
+//     { prop1: 102, prop2: false, prop3: 'hello 2'},
+//     { prop1: 103, prop2: true, prop3: 'hello 3'},
+//     { prop1: 104, prop2: false, prop3: 'hello 4'},
+// ];
+
+// // for (let x of a) {
+// //     console.log(x);
+// // }
+
+// a.forEach((element) => {
+//     element.prop4 = null;
+// });
+
+// // console.log(a);
+
+// const b = a.map((el) => {
+//     // el.prop4 = undefined;
+//     return { ...el, prop4: undefined };
+// });
+
+// const c = a.filter((e) => {
+//     return e.prop2 === true
+// });
+
+// console.log(c);
+
+// // console.log(a);
+// // console.log(b);
+
+
+
+// // var x = { prop: 1, prop2: 'a'};
+// // var y = x;
+// // x.prop2 = 'b';
+// // console.log(y)
+
+// // var x =  'a';
+// // var y = x;
+// // x = 'b';
+// // console.log(y);
+
+// function isAnagram(word1, word2) {
+//     return word1.split('').sort().join('') === word2.split('').sort().join('')
+// }
+
+// // console.log(isAnagram('cat', 'act'));
+
+
+// // function timer() {
+// //     for (var i = 0; i<=3 ; i++) {
+// //         setTimeout(() => {
+// //             console.log(i)
+// //         }, 100)
+// //     }
+// // }
+
+// // timer();
